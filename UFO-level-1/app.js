90fdf89011c87fab887be559312f082aee11183c
Claudia Ibarra <Claudi@Claudias-MBP.lan>
2020-03-16 18:27:31 -0500

    third commit

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,37 +1,95 @@
-const tableData = data
-const tbody = d3.select('tbody')
+// from data.js
+const tableData = data;
+const input = d3.select("input")
+const button = d3.select("button")
 
-function buildTable(data) {
-    tbody.html('')  // Clear existing data
+const table = d3.select("#ufo-table")
+table.html("")
 
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
+const thead = table.append("thead")
+const head_row = thead.append("tr")
+const tbody = table.append("tbody")
+
+head_row.append("th").text("date")
+head_row.append("th").text("city")
+head_row.append("th").text("state")
+head_row.append("th").text("country")
+head_row.append("th").text("shape")
+head_row.append("th").text("duration")
+head_row.append("th").text("comments")
+
+data.forEach(record => {
+    let row = tbody.append("tr")
+    row.append("td").text(record.datetime)
+    row.append("td").text(record.city)
+    row.append("td").text(record.state)
+    row.append("td").text(record.country)
+    row.append("td").text(record.shape)
+    row.append("td").text(record.duration)
+    row.append("td").text(record.comments)
+})
+
+
+const handler = function(){
+
+    let filterCond = input.property("value")
+
+    if (filterCond === "") {
+        
+        table.html("")
+
+        const thead = table.append("thead")
+        const head_row = thead.append("tr")
+        const tbody = table.append("tbody")
+
+        head_row.append("th").text("date")
+        head_row.append("th").text("city")
+        head_row.append("th").text("state")
+        head_row.append("th").text("country")
+        head_row.append("th").text("shape")
+        head_row.append("th").text("duration")
+        head_row.append("th").text("comments")
+
+        data.forEach(record => {
+            let row = tbody.append("tr")
+            row.append("td").text(record.datetime)
+            row.append("td").text(record.city)
+            row.append("td").text(record.state)
+            row.append("td").text(record.country)
+            row.append("td").text(record.shape)
+            row.append("td").text(record.duration)
+            row.append("td").text(record.comments)
         })
-    })
+    } else {
+
+        const table = d3.select("#ufo-table")
+        table.html("")
+
+        const thead = table.append("thead")
+        const head_row = thead.append("tr")
+        const tbody = table.append("tbody")
+
+        head_row.append("th").text("date")
+        head_row.append("th").text("city")
+        head_row.append("th").text("state")
+        head_row.append("th").text("country")
+        head_row.append("th").text("shape")
+        head_row.append("th").text("duration")
+        head_row.append("th").text("comments")
+
+        data.filter(record => record.datetime === filterCond).forEach(record => {
+            let row = tbody.append("tr")
+            row.append("td").text(record.datetime)
+            row.append("td").text(record.city)
+            row.append("td").text(record.state)
+            row.append("td").text(record.country)
+            row.append("td").text(record.shape)
+            row.append("td").text(record.duration)
+            row.append("td").text(record.comments)
+
+        })
+    }
 }
 
-const handleClick = () => {
-    d3.event.preventDefault()
-
-    const date = d3.select('#datetime').property('value')
-    let filteredData = tableData;
-
-    if (date)
-        filteredData = filteredData.filter(row => row.datetime === date)
-    
-    tbody.html('')  // Clear existing data
-
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
-    })
-}
-
-d3.selectAll('#filter-btn').on('click', handleClick)
-// buildTable(tableData)
+input.on("change", handler)
+input.on("click", handler)
